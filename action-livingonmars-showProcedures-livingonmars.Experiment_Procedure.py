07788164822c10bf8492101b5bd3833953ea98a1
Rita <44863024+ritafx@users.noreply.github.com>
2020-01-16 11:46:05 +0100

    bug fix

--- action-livingonmars-showProcedures-livingonmars.Experiment_Procedure.py
+++ action-livingonmars-showProcedures-livingonmars.Experiment_Procedure.py
@@ -358,7 +358,7 @@ def proceduresListOutput():
 
     # create dialogue output for VUI
     output_message = "I have found {} Procedures. You can wake me up, and tell me a number to choose a procedure. At any time, you can ask me to repeat, to listen to the information again. You can also ask me to cancel, and I will restart. Here are the procedures. {} ".format(
-        total_procedures, procedure_list)
+        total_procedures, procedures_list)
 
     if isConnected():
         # request to GUI API to show the list on the screen
